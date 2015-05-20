60d34e4a1c2dcee6760091581deadf4ee94ace90
Fisseha Abebe <abebefisseha5@gmail.com>
2015-05-20 17:59:50 +0300

    First Commit!

---
+++ EthiopianDates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmharicDateConverter
+{
+    public static class EthiopianDates
+    {
+       
+        public static string ToEthiopianDateFormat(this DateTime date)
+        {
+            if (date.Date.Equals(DateTime.Parse("1/1/0001")))
+                return string.Empty;
+
+                DateHelper ethiopianDate = new DateHelper(date);
+                return ethiopianDate.ToLongDateString();
+           
+        }
+
+    }
+}
