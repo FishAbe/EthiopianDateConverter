18f0e4b56f833ce0e3b92fb947c942d9ffe1ec06
Fisseha Abebe <abebefisseha5@gmail.com>
2015-06-10 15:07:06 +0300

    Changed .Net Framework version from 4.5  to 2.0 since SSRS Doesn't support .net framework version greater than 2.0

--- DateHelper.cs
+++ DateHelper.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text;
 using System.Globalization;
-using System.Threading.Tasks;
+
 
 namespace AmharicDateConverter
 {
--- EthiopianDates.cs
+++ EthiopianDates.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace AmharicDateConverter
 {
     public static class EthiopianDates
     {
        
-        public static string ToEthiopianDateFormat(this DateTime date)
+        public static string ToEthiopianDateFormat(DateTime date)
         {
             if (date.Date.Equals(DateTime.Parse("1/1/0001")))
                 return string.Empty;
